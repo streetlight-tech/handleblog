e3ebf945f25ff8bebef15fd5a9cd5f3b7d59ff02
Doug Lampe <lampedoug@yahoo.com>
2022-10-02 18:03:40 -0400

    feat: add post query

--- src/IPostProvider.ts
+++ src/IPostProvider.ts
@@ -1,6 +1,8 @@
 import { IPost } from './IPost';
+import { IPostQuery } from './IPostQuery';
 
 export interface IPostProvider {
-  list(): Promise<IPost[]>;
+  list(query?: IPostQuery): Promise<IPost[]>;
   get(key: string): Promise<IPost>;
+  save(post: IPost): Promise<void>;
 }
