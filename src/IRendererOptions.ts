6edd8ec019d637d24991343a276695e76a50b0aa
Doug Lampe <douglampe@users.noreply.github.com>
2022-10-02 18:06:31 -0400

    feat: add markdown, page config, and new post fields (#12)

--- src/IRendererOptions.ts
+++ src/IRendererOptions.ts
@@ -1,7 +1,9 @@
+import { IPageConfig } from "./IPageConfig";
 import { IPostProvider } from "./IPostProvider";
 import { ITemplateProvider } from "./ITemplateProvider";
 
 export interface IRendererOptions {
   postProvider: IPostProvider;
   templateProvider: ITemplateProvider;
+  pageConfig: IPageConfig;
 }
--- src/Renderer.ts
+++ src/Renderer.ts
@@ -1,54 +1,80 @@
 import Handlebars from 'handlebars';
-import { IPostProvider } from 'IPostProvider';
-import { IRendererOptions } from 'IRendererOptions';
-import { ITemplateProvider } from 'ITemplateProvider';
+import { marked } from 'marked';
+import { IPageConfig } from './IPageConfig';
+import { IPostProvider } from './IPostProvider';
+import { IPost } from './IPost';
+import { IRendererOptions } from './IRendererOptions';
+import { ITemplateProvider } from './ITemplateProvider';
+import { IPostQuery } from './IPostQuery';
 
 export class Renderer {
   private postProvider: IPostProvider;
   private templateProvider: ITemplateProvider;
+  private pageConfig: IPageConfig;
 
   constructor(options: IRendererOptions) {
     this.postProvider = options.postProvider;
     this.templateProvider = options.templateProvider;
-
+    this.pageConfig = options.pageConfig;
     Handlebars.registerHelper('formatDate', this.formatDate)
   }
 
   public async render<T>(template: string, content: T): Promise<string> {
+    console.log(content);
+    
     const compiled = Handlebars.compile(template);
+
     return compiled(content);
   }
 
-  public async renderHome<T>(): Promise<string> {
+  public async renderHime<T>(query: IPostQuery): Promise<string> {
     const template = await this.templateProvider.getHomeTemplate();
-    const posts = await this.postProvider.list();
-    const compiled = Handlebars.compile(template);
+    const posts = await this.postProvider.list(query);
 
-    return compiled({ posts });
+    posts.map(p => Renderer.parseBody(p));
+
+    return this.render(template, { 
+      posts, 
+      ...this.pageConfig,
+    });
   }
 
-  public async renderList<T>(): Promise<string> {
+  public async renderList<T>(query: IPostQuery): Promise<string> {
     const template = await this.templateProvider.getListTemplate();
-    const posts = await this.postProvider.list();
-    const compiled = Handlebars.compile(template);
+    const posts = await this.postProvider.list(query);
 
-    return compiled({ posts });
+    posts.map(p => Renderer.parseBody(p));
+
+    return this.render(template, { 
+      posts, 
+      ...this.pageConfig,
+    });
   }
 
   public async renderPost<T>(key: string): Promise<string> {
     const template = await this.templateProvider.getLPostTemplate();
     const post = await this.postProvider.get(key);
-    const compiled = Handlebars.compile(template);
 
-    return compiled(post);
+    Renderer.parseBody(post);
+
+    return this.render(template, {
+      ...post,
+      ...this.pageConfig,
+    });
   }
 
-  public formatDate(date: Date) {
+  public static parseBody(post: IPost) {
+    post.body = marked.parse(post.body);
+  }
+
+  public formatDate(dateValue: string) {
+    const date = new Date(dateValue);
     const options: Intl.DateTimeFormatOptions = { 
       year: 'numeric', 
       month: 'short', 
       day: 'numeric',
     };
+    
     return (new Intl.DateTimeFormat('en-US', options)).format(date);
   }
 }
