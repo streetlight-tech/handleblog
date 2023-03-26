88c9187cf3893872fd557fd5ce9cc7b53bccd990
Doug Lampe <douglampe@users.noreply.github.com>
2023-03-26 10:14:02 -0400

    test: test post after list

--- test/unit/Renderer.spec.ts
+++ test/unit/Renderer.spec.ts
@@ -256,6 +256,89 @@ describe('Renderer', () => {
 
       expect(result).toBe('post-1:Blog post 1:Bloggy Blogerton:Jan 1, 2000::<p>This is a blog post with an image <img src="https://content.here.com/image.png" alt="image" /></p>\n/Posts about Blogs[blog,post,]');
     });
+
+    it('should render a single post even afte list', async() => {
+      const postProvider: IPostProvider = {
+        list: (query?: IPostQuery): Promise<IPost[]> => {
+          return Promise.resolve([
+            {
+              key: 'post-1',
+              title: 'Blog post 1',
+              author: 'Bloggy Blogerton',
+              date: new Date(2000, 0, 1),
+              body: 'This is a blog post',
+              category: 'Posts about Blogs',
+              tags: [ 'blog', 'post' ],
+            },
+            {
+              key: 'post-2',
+              title: 'Blog post 2',
+              author: 'Bloggy Blogerton',
+              date: new Date(2000, 0, 2),
+              body: 'This is another blog post',
+              category: 'Posts about Blogs',
+              tags: [ 'blog', 'post' ],
+            }
+          ]);
+        },
+        get: (key) => {
+          return Promise.resolve({ key, title: 'foo' });
+        },
+        save: () => { return Promise.resolve(); },
+      };
+
+      const renderer = new Renderer({ 
+        postProvider, 
+        templateProvider,
+        pageConfig: {
+          pageTitle: 'Title',
+          root: 'https://here.com',
+          contentRoot: 'https://content.here.com',
+          social: [],
+        } 
+      });
+
+
+      mockGetList.mockResolvedValue('{{#posts}}{{key}}:{{title}}:{{author}}:{{formatDate date}}::{{{body}}}/{{category}}[{{#tags}}{{this}},{{/tags}}]{{/posts}}');
+      const result = await renderer.renderList();
+
+      expect(result).toBe('post-1:Blog post 1:Bloggy Blogerton:Jan 1, 2000::<p>This is a blog post</p>\n/Posts about Blogs[blog,post,]post-2:Blog post 2:Bloggy Blogerton:Jan 2, 2000::<p>This is another blog post</p>\n/Posts about Blogs[blog,post,]');
+
+      const postProvider: IPostProvider = {
+        list: () => {
+          return Promise.resolve([]);
+        },
+        get: (key) => {
+          return Promise.resolve({
+            key,
+            title: 'Blog post 1',
+            author: 'Bloggy Blogerton',
+            date: new Date(2000, 0, 1),
+            body: 'This is a blog post with an image ![image](image.png)',
+            category: 'Posts about Blogs',
+            tags: [ 'blog', 'post' ],
+          });
+        },
+        save: () => { return Promise.resolve(); },
+      };
+
+      const renderer = new Renderer({ 
+        postProvider, 
+        templateProvider,
+        pageConfig: {
+          pageTitle: 'Title',
+          root: 'https://here.com',
+          contentRoot: 'https://content.here.com',
+          social: [],
+        } 
+      });
+      
+      mockGetPost.mockResolvedValue('{{key}}:{{title}}:{{author}}:{{formatDate date}}::{{{body}}}/{{category}}[{{#tags}}{{this}},{{/tags}}]');
+      const result = await renderer.renderPost('post-1');
+
+      expect(result).toBe('post-1:Blog post 1:Bloggy Blogerton:Jan 1, 2000::<p>This is a blog post with an image <img src="https://content.here.com/image.png" alt="image" /></p>\n/Posts about Blogs[blog,post,]');
+    });
+
   });
 
   describe('stripMarkdown', () => {
